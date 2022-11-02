125eb1b1ae319e9c215517b789c35d2e0f1f5b45
101arrowz <arjunbarrett@gmail.com>
2022-11-02 11:31:52 -0700

    v0

---
+++ index.js
@@ -0,0 +1,10 @@
+var links = [
+  'https://docs.google.com/forms/d/e/1FAIpQLSf1MQRr1V3YMfUVPUQt-sCgHbSzV19Ii3UNozP908Vhfi8-KA/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLSc_QhwvcS58RMeX1FkZvVxfNupBfHpNduMse543M1nEsh6lbw/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLSeW7VDAMu3Y-2nz02BNwYP6fruiYoM5nbfwHBkZKw45623fEA/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLSfH47iwHQEIlePCGjpxcpBNHfOCwaYwhPEZ1nk9jhq-4oO92w/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLSek9ZLRJdsBuvITQJr2njnvf-irkf1JHO-O4_RW9K5tEuK0fQ/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLSfXXD6lovY4dymoyR37uv0Jk0zZJsXuUvnEZfbBtYs9spJ4eQ/viewform?usp=sf_link'
+];
+
+window.location.href = links[Math.floor(Math.random() * links.length)];
